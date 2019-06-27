6b2783eb134b37d142cb188b6e3e13c65f381782
Danny Rooker <dannyrooks@gmail.com>
2019-06-27 00:59:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ language = "Python"
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-name = "Edwin"
+name = ""
 language = "Ruby"
 
 def introduction_with_language_optional(name, language)
