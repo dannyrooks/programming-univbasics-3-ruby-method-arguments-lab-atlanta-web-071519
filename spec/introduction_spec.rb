05a61fbe754785737d83533bbf1374c467c35e20
Danny Rooker <dannyrooks@gmail.com>
2019-06-27 01:03:04 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -18,7 +18,7 @@ describe "#introduction_with_language_optional" do
   it "takes in two arguments, a name and a language, and language defaults to Ruby" do
     expect{introduction_with_language_optional("Edwin")}.to output("Hi, my name is Edwin and I am learning to program in Ruby.\n").to_stdout
   end
-
+end
 
 describe "#introduction_with_language_optional" do
   it "takes in two arguments, a name and a language, language can be optional or defined" do
